7d77fc34eb543debe3076a25737bf31a40577822
Karim Bakkali <kbldn@live.co.uk>
2020-03-16 23:27:25 +0000

    Added a loop to show all students and a button to download their CV's

--- spart-front-end/src/App.js
+++ spart-front-end/src/App.js
@@ -1,53 +1,60 @@
 import React from "react";
-import { Spinner, Container, Row, Col, ResponsiveEmbed } from "react-bootstrap/";
+import { Container, Row, Col } from "react-bootstrap/";
 import ConsultantCard from "./Components/ConsultantCard";
 import NavBar from "./Components/NavBar";
-import Video from 'react-native-video'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      students: [],
-      isLoaded: false
-    };
-  }
+		this.state = {
+			items: [],
+			isLoaded: false
+		};
+	}
 
-  componentDidMount() {
-    fetch("http://localhost:56767/api/consultants")
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          students: json,
-          isLoaded: true
-        });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+	componentDidMount() {
+		fetch("http://localhost:56767/api/consultants")
+			.then(res => res.json())
+			.then(json => {
+				this.setState({
+					items: json,
+					isLoaded: true
+				});
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
 
-  /**
-   * render
-   *
-   * Render UI
-   */
-  render() {
-    const { students } = this.state;
-    return (
-      <div>
-        <NavBar />
+	/**
+	 * render
+	 *
+	 * Render UI
+	 */
+	render() {
+		const list = this.state.items;
 
-        <Container>
-          <Row>
-            <Col><Video></Video></Col>
-            <Col><ConsultantCard handleStudents={this.state.students} /></Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<NavBar />
+				{list.map(item => (
+					<Container key={item.consultantId}>
+						<Row>
+							<Col>
+								<video width="500" height="300" controls>
+									<source src="/movie.mp4" type="video/mp4" />
+								</video>
+							</Col>
+							<Col>
+								<ConsultantCard handleStudents={item} />
+							</Col>
+						</Row>
+					</Container>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
--- spart-front-end/src/Components/ConsultantCard.jsx
+++ spart-front-end/src/Components/ConsultantCard.jsx
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
-import App from '../App';
-
+import React, { Component } from "react";
+import { Card, Button } from "react-bootstrap";
 
 class ConsultantCard extends React.Component {
-    constructor(props){
-        super(props);
+	constructor(props) {
+		super(props);
+	}
 
-    }
-    render() {
-      const list = this.props.handleStudents;
-        return (
-            <Card
-          bg="light"
-          variant="light"
-          style={{ alignItems: "center", width: "18rem" }}
-        >
-          <Card.Img variant="top" src="https://www.thesprucepets.com/thmb/ngEcQBHHXcHYXh8PPTvGttRxYN0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/high-angle-view-of-chimpanzee-in-forest-769784687-5b1e76d63de4230037ce6f9d.jpg" />
-          <Card.Body>
-            <Card.Title>
-              {list
-                .filter(s => s.consultantId === 1)
-                .map(st => (
-                  <li>
-                    {" "}
-                    {st.firstName} {st.lastName}
-                  </li>
-                ))}
-            </Card.Title>
-            <Card.Text>
-              {list
-                .filter(s => s.consultantId === 1)
-                .map(st => (
-                  <li> {st.bio}</li>
-                ))}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        );
-    }
+	downloadCv(item) {
+		item = this.props.handleStudents;
+		return item.cvLink + "&download=1";
+	}
+
+	render() {
+		const item = this.props.handleStudents;
+		return (
+			<Card
+				className="mt-2"
+				bg="light"
+				variant="light"
+				style={{ alignItems: "center", width: "18rem" }}
+			>
+				<Card.Img
+					variant="top"
+					src="https://www.thesprucepets.com/thmb/ngEcQBHHXcHYXh8PPTvGttRxYN0=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/high-angle-view-of-chimpanzee-in-forest-769784687-5b1e76d63de4230037ce6f9d.jpg"
+				/>
+				<Card.Body>
+					<Card.Title>
+						<li style={{ listStyleType: "none" }}>
+							{"Name: "}
+							{item.firstName} {item.lastName}
+						</li>
+					</Card.Title>
+					<Card.Text>
+						<li style={{ listStyleType: "none" }}>
+							{"Bio: "} {item.bio}
+						</li>
+						<Button href={item.cvLink + "&download=1"}>Download CV</Button>{" "}
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		);
+	}
 }
 
 export default ConsultantCard;
