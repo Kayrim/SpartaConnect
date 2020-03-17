4808cbbc5e39cba6926da98a15948b7e4852771b
tim8642 <59577781+tim8642@users.noreply.github.com>
2020-03-17 14:20:19 +0000

    first initial
    Added front end:
    search
    cards
    styling

---
+++ SpartaConnectMVC/Controllers/ConsultantsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SpartaConnect_API.Models;
+
+namespace SpartaConnectMVC.Controllers
+{
+    public class ConsultantsController : Controller
+    {
+        private readonly SpartaDBContext _context;
+
+        public ConsultantsController(SpartaDBContext context)
+        {
+            _context = context;
+        }
+
+        
+        // GET: Consultants
+       /* public async Task<IActionResult> Index()
+        {          
+            return View(await _context.Consultant.ToListAsync());
+        }*/
+
+        // Get: Consultants based of Search criteria
+        public async Task<IActionResult> Index(string searching)
+        {
+            var spartaDBContext = _context.Consultant.Where(x => x.FirstName.StartsWith(searching) || searching == null);
+            return View(await spartaDBContext.ToListAsync());
+        }
+
+        // GET: Consultants/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultant = await _context.Consultant
+                .Include(c => c.Course)
+                .FirstOrDefaultAsync(m => m.ConsultantId == id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+
+            return View(consultant);
+        }
+
+        // GET: Consultants/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId");
+            return View();
+        }
+
+        // POST: Consultants/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ConsultantId,LastName,FirstName,CourseId,Photo,Bio,University,CvLink,VideoLink")] Consultant consultant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(consultant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId", consultant.CourseId);
+            return View(consultant);
+        }
+
+        // GET: Consultants/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultant = await _context.Consultant.FindAsync(id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId", consultant.CourseId);
+            return View(consultant);
+        }
+
+        // POST: Consultants/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ConsultantId,LastName,FirstName,CourseId,Photo,Bio,University,CvLink,VideoLink")] Consultant consultant)
+        {
+            if (id != consultant.ConsultantId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(consultant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConsultantExists(consultant.ConsultantId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CourseId"] = new SelectList(_context.Course, "CourseId", "CourseId", consultant.CourseId);
+            return View(consultant);
+        }
+
+        // GET: Consultants/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var consultant = await _context.Consultant
+                .Include(c => c.Course)
+                .FirstOrDefaultAsync(m => m.ConsultantId == id);
+            if (consultant == null)
+            {
+                return NotFound();
+            }
+
+            return View(consultant);
+        }
+
+        // POST: Consultants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var consultant = await _context.Consultant.FindAsync(id);
+            _context.Consultant.Remove(consultant);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConsultantExists(int id)
+        {
+            return _context.Consultant.Any(e => e.ConsultantId == id);
+        }
+    }
+}
